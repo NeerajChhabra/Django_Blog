d3c33fba9237227657da1c4cd33e54ae05642d24
Neeraj-Chhabra <neeraj.chhabra@dal.ca>
2020-07-03 15:57:38 -0300

    Profile functionality updated --> Create profile on user creation

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views  as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +29,9 @@ urlpatterns = [
     path('profile/', user_views.profile, name ="profile"),
     
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
